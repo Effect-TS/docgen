6ac4d769ad3243bd0b9378201e7b0727c632b755
Sebastian Lorenz <fubhy@fubhy.com>
2023-11-15 18:21:01 +0100

    copy schema.json to dist folder

--- scripts/copy-package-json.ts
+++ scripts/copy-package-json.ts
@@ -1,17 +1,17 @@
-import * as FileSystem from "@effect/platform-node/FileSystem"
-import { Effect, ReadonlyRecord } from "effect"
-import * as path from "node:path"
+import { FileSystem, Path } from "@effect/platform-node"
+import { Effect, Layer, ReadonlyRecord } from "effect"
 
 const excludedPrefixes = ["@effect", "effect", "chalk", "tsconfck", "ts-morph"]
 
 const excludeEffectPackages = (deps: Record<string, string>): Record<string, string> =>
   ReadonlyRecord.filter(deps, (_, k) => !excludedPrefixes.some((_) => k.startsWith(_)))
 
-const pathTo = path.join("dist", "package.json")
-
 const program = Effect.gen(function*(_) {
-  console.log(`[Build] Copying package.json to ${pathTo}...`)
   const fs = yield* _(FileSystem.FileSystem)
+  const path = yield* _(Path.Path)
+  yield* _(Effect.log(`[Build] Copying schema.json ...`))
+  yield* _(fs.copyFile("schema.json", path.join("dist", "schema.json")))
+  yield* _(Effect.log(`[Build] Copying package.json ...`))
   const json: any = yield* _(fs.readFileString("package.json"), Effect.map(JSON.parse))
   const pkg = {
     name: json.name,
@@ -31,10 +31,10 @@ const program = Effect.gen(function*(_) {
     tags: json.tags,
     keywords: json.keywords
   }
-  yield* _(fs.writeFileString(pathTo, JSON.stringify(pkg, null, 2)))
-  console.log("[Build] Build completed.")
+  yield* _(fs.writeFileString(path.join("dist", "package.json"), JSON.stringify(pkg, null, 2)))
+  yield* _(Effect.log("[Build] Build completed."))
 }).pipe(
-  Effect.provide(FileSystem.layer)
+  Effect.provide(Layer.merge(FileSystem.layer, Path.layerPosix))
 )
 
 Effect.runPromise(program).catch(console.error)
